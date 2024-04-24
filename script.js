351997c99fb3d5f89fa2d85a102b6f3f203877f4
naknamu <49525720+naknamu@users.noreply.github.com>
2024-04-24 10:35:12 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const number = document.querySelector("span");
-const button = document.querySelector("button");
-
-let count = 0;
-
-button.addEventListener("click", () => {
-  count++;
-  number.textContent = count;
-});
